9498ac2166302c716c7842e8e1a6fcabd60ba2a1
Samuel Revaldo Tjahyadi <samuelrev20@gmail.com>
2026-07-14 12:32:08 +0700

    Update: Renewing Database and CORS for deployment

--- backend/admin/middleware.go
+++ backend/admin/middleware.go
@@ -1,10 +1,17 @@
 package admin
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		allowedOrigin := os.Getenv("FRONTEND_URL")
+		if allowedOrigin == "" {
+			allowedOrigin = "*"
+		}
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
