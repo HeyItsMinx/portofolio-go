d3cdf5618343083d37d1fdae6da8b3c983485f07
Samuel Revaldo Tjahyadi <samuelrev20@gmail.com>
2026-07-24 12:27:02 +0700

    Update: Scrollbar

--- frontend/src/components/layout/PublicLayout.jsx
+++ frontend/src/components/layout/PublicLayout.jsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import TerminalFooter from '../footer/TerminalFooter';
 import Navbar from '../nav/Navbar';
 import StatusTicker from '../ticker/StatusTicker';
 
 export default function PublicLayout({ children }) {
+  useEffect(() => {
+    document.documentElement.classList.add('public-scrollbar');
+    return () => document.documentElement.classList.remove('public-scrollbar');
+  }, []);
+
   return (
     <div className="bg-black min-h-screen flex flex-col">
       <Navbar />
